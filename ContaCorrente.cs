eb5b8b716165dd9b2646dfca826ba585169e72da
RafahsCunha <rafaelhsc20@gmail.com>
2022-12-04 21:44:01 -0300

    Criando a classe ContaCorrente

---
+++ ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_Alura_Orientacao_a_Objetos
+{
+    class ContaCorrente : IRegras
+    {
+        public ContaCorrente()
+        {
+            Console.WriteLine("******* BANCO JAAR *******");
+        }
+        private string nome;
+        private string agencia;
+        private string conta_corrente;
+        private double saldo;
+
+
+        public void usuario(string nome, string agencia, string conta_corrente)
+        {
+            this.nome = nome;
+            this.agencia = agencia;
+            this.conta_corrente = conta_corrente;
+
+            Console.WriteLine("Titular: " + this.nome + "\nAgencia: " + this.agencia +
+               "\nConta Corrente: " + this.conta_corrente);
+        }
+        // Deposito obrigatório da interface IRegras
+        public double primeiroDepositar(double valor)
+        {
+            return this.saldo = valor;
+            Console.WriteLine("Primeiro depósito efetuado no valor de R$ " + this.saldo);
+        }
+
+        public double depositar(double valorDepositado)
+        {
+            return this.saldo += valorDepositado;
+
+        }
+
+        public void sacar(double valorDoSaque)
+        {
+            if (valorDoSaque <= this.saldo)
+            {
+                this.saldo -= valorDoSaque;
+                Console.WriteLine("Saque realizado com sucesso.. Valor: R$" + valorDoSaque);
+            }
+            else
+            {
+                Console.WriteLine("Saldo Insuficiente para Saque..");
+            }
+
+        }
+        public void transferir(double valorTrasferido, ContaCorrente destino)
+        {
+            if (valorTrasferido <= this.saldo)
+            {
+                this.saldo -= valorTrasferido;
+                destino.saldo += valorTrasferido;
+                Console.WriteLine("Transferencia realizada com sucesso.. Valor: " + valorTrasferido)
+            }
+            else
+            {
+                Console.WriteLine("Transferencia não realizada, valor indisponível..");
+            }
+        }
+
+        public void consultaSaldo()
+        {
+            Console.WriteLine("Saldo disponivel R$ " + this.saldo);
+        }
+    }
+}
