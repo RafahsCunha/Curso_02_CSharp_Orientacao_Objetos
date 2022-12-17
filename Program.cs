e541b5c167db97003bc706210c81121b76674979
RafahsCunha <rafaelhsc20@gmail.com>
2022-12-17 15:23:40 -0300

    Corrigindo a Program

--- Program.cs
+++ Program.cs
@@ -13,14 +13,14 @@ class Program
         ContaCorrente conta1 = new ContaCorrente(); // objeto conta1
         conta1.titular = cliente1; // objeto conta1.titular recebe o objeto cliente1. OBS titular é um objeto do tipo Cliente
 
-        conta1.usuarioConta(cliente1, "Itaú-1234", "12334-4");
+        conta1.usuarioConta(cliente1, 1234, "12334-4");
 
         conta1.setprimeiroDeposito(100);
 
         Console.WriteLine("Titular: "+conta1.titular.nome +
             "\nCPF: " + conta1.titular.cpf +
             "\nProfissao: "+ conta1.titular.profissao + 
-            "\nAgencia: "+conta1.agencia +
+            "\nAgencia: "+conta1.numero_agencia +
             "\nConta Corrente: "+conta1.conta_corrente + 
             "\nSaldo: "+ conta1.getSaldo());
 
@@ -33,14 +33,14 @@ class Program
         ContaCorrente conta2 = new ContaCorrente();
         conta2.titular = cliente2;
 
-        conta2.usuarioConta(cliente2, "9875", "4567-8");
+        conta2.usuarioConta(cliente2, 9875, "4567-8");
 
         conta2.setprimeiroDeposito(200);
 
         Console.WriteLine("Titular: " + conta2.titular.nome +
             "\nCPF: " + conta2.titular.cpf +
             "\nProfissao: " + conta2.titular.profissao +
-            "\nAgencia: " + conta2.agencia +
+            "\nAgencia: " + conta2.numero_agencia +
             "\nConta Corrente: " + conta2.conta_corrente +
             "\nSaldo: " + conta2.getSaldo());
 
