cc1d489eb193eedbc61f52750b9bc209882e9b59
RafahsCunha <rafaelhsc20@gmail.com>
2022-12-18 07:26:54 -0300

    Propriedades Auto Implementadas

--- Titular/Cliente.cs
+++ Titular/Cliente.cs
@@ -9,16 +9,28 @@ namespace Alura.Titular
 {
     public class Cliente
     {
-        public string nome;
-        public string cpf;
-        public string profissao;
+        //public string nome;
+        //public string cpf;
+        //public string profissao;
 
-        public void criaCliente(string nome, string cpf, string profissao)
+        //public void criaCliente(string nome, string cpf, string profissao)
+        //{
+        //    //this.nome = nome;
+        //    //this.cpf = cpf;
+        //    //this.profissao = profissao;
+
+            //CODE SNIPED prop tab + tab recurso para criar o escopo de uma propriedade auto implementada
+
+        // Propriedades publicas substituindo os atributos e métodos acima
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+
+        public void cliente(string nome, string cpf, string profissao)
         {
-            this.nome = nome;
-            this.cpf = cpf;
-            this.profissao = profissao;
+            this.Nome = nome;
+            this.Cpf = cpf; 
+            this.Profissao = profissao;
         }
-
     }
 }
